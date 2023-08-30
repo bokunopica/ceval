cabe50c184f73cfe51c65b227adfdc35115c6e6c
qq <qianq9720@163.com>
2023-08-30 11:43:55 +0800

    修改脚本

--- eval_all.sh
+++ eval_all.sh
@@ -1,6 +1,6 @@
 python code/evaluator_series/eval_all.py \
-    --model_name 'chatglm' \
-    --ckpt_path '/home/qianq/mycodes/VisualGLM-6B/checkpoints/2-gpus/finetune-visualglm-6b-ptuning-pre-128' \
+    --model_name 'finetuned_chatglm' \
+    --ckpt_path '/home/qianq/mycodes/VisualGLM-6B/checkpoints/2-gpus/finetune-visualglm-6b-lora-layer[0,14]' \
     --cuda_device '1' \
     --few_shot \
     --ntrain 1
